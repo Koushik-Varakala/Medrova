029529400edb6c2ea28adf471729dd1eaa9be57b
Koushik Varakala <koushikvarakala@gmail.com>
2026-05-14 14:55:33 +0530

    vercel deployment fix 3

--- components/clinic/PostShiftForm.tsx
+++ components/clinic/PostShiftForm.tsx
@@ -27,9 +27,12 @@ interface RazorpayCheckoutOptions {
   name: string;
   description: string;
   order_id: string;
-  handler: () => void;
+  handler: (response: { razorpay_payment_id: string; razorpay_order_id: string; razorpay_signature: string }) => void;
   theme: {
     color: string;
+  };
+  modal?: {
+    ondismiss?: () => void;
   };
 }
 
